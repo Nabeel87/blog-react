b8e79af5b262a053bce82b4e77920628eb2e4e81
Nabell87 <nabilsabir786@gmail.com>
2023-08-05 12:19:07 +0500

    create page
    single page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import TopBar from "./component/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
 
       <TopBar />
-      <Home />
+      <Single />
 
     </>
   );
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,11 @@
+import "./single.css";
+import Sidebar from "../../component/sidebar/Sidebar";
+
+export default function Single() {
+    return (
+        <div className="single">
+            {/* post */}
+            <Sidebar />
+        </div>
+    )
+}
