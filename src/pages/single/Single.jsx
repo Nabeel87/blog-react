8975e2b677f5f46aa1e38689a472d850dd0bf020
Nabell87 <nabilsabir786@gmail.com>
2023-08-05 12:38:03 +0500

    single post
    single post component for single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,10 +1,11 @@
 import "./single.css";
 import Sidebar from "../../component/sidebar/Sidebar";
+import SinglePost from "../../component/singlePost/SinglePost";
 
 export default function Single() {
     return (
         <div className="single">
-            {/* post */}
+            <SinglePost />
             <Sidebar />
         </div>
     )
