6fc33c1187111a51c8a9663fce676ece0502de79
Nabell87 <nabilsabir786@gmail.com>
2023-08-05 13:52:38 +0500

    write page
    write page for publishing post

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import TopBar from "./component/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
 
       <TopBar />
-      <Single />
+      <Write />
 
     </>
   );
