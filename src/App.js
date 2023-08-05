3864b5afc97ed648156a1a63513d8996c8f10d75
Nabell87 <nabilsabir786@gmail.com>
2023-08-05 14:10:44 +0500

    register page
    jsx and css ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import TopBar from "./component/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -10,7 +12,7 @@ function App() {
     <>
 
       <TopBar />
-      <Settings />
+      <Register />
 
     </>
   );
