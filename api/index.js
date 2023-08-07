42d202f6756146d83f55d1e1f7eebf296e242036
Nabell87 <nabilsabir786@gmail.com>
2023-08-07 13:40:32 +0500

    routes, models, mongoDB connection
    create files and folders for routes and models
    and also creating a connection with MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
-
 const app =express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB")
+      } catch (error) {
+        throw error;
+      }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+  console.log("mongoDB disconnected...")
+})
 
 app.listen("5000", ()=>{
+    connect();
     console.log("backend is running");
 })
