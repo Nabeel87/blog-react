f0f2ffacd7aa6176de640328fe8acc8be77a6e70
Nabell87 <nabilsabir786@gmail.com>
2023-08-08 19:56:03 +0500

    login user
    login user and check password and user name

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const bcrypt = require('bcrypt');
 
 //REGISTER
-router.post("/register", async(req, res)=>{
+router.post("/register", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
@@ -21,4 +21,22 @@ router.post("/register", async(req, res)=>{
     }
 })
 
-module.exports =router
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Wrong Username...!");
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong Username or Password");
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router
