f6acb4dc5c1b62cb7d6092ec95c4166bb987fecc
Nabell87 <nabilsabir786@gmail.com>
2023-08-09 21:06:05 +0500

    delete post
    delete post not working check later

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -39,22 +39,23 @@ router.put("/:id", async (req, res) => {
   });
 
   //DELETE POST
-router.delete("/:id", async (req, res) => {
+  router.delete("/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       if (post.username === req.body.username) {
         try {
-          await post.delete();
-          res.status(200).json("Post has been deleted...");
+            await post.delete();
+          res.status(200).json("post has been deleted");
         } catch (err) {
           res.status(500).json(err);
         }
       } else {
-        res.status(401).json("You can delete only your post!");
+        res.status(401).json("You can update only your post!");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
 
 module.exports = router;
