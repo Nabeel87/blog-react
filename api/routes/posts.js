7504a11cf014e694a460d2bd0f29d8821f9c5b47
Nabell87 <nabilsabir786@gmail.com>
2023-08-09 20:45:49 +0500

    changing....

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,15 +3,14 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 
 //CREATE POST
-router.post("/", async (req, res, next) => {
+router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch (err) {
-    next(err);
+    res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
