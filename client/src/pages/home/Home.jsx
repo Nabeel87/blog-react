f952ae9cc1fddfd25304ee434be54c1636eadf20
Nabell87 <nabilsabir786@gmail.com>
2023-08-15 14:14:22 +0500

    ignore

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import Header from "../../component/header/Header";
 import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
 import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" );
+      setPosts(res.data);
+      console.log(res.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
-  )
+  );
 }
