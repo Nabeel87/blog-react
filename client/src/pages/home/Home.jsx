a13966651e10ffb459e0a3adc0cf31a69f555bef
Nabell87 <nabilsabir786@gmail.com>
2023-08-16 12:23:53 +0500

    search by user
    add use location and search by user

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -10,13 +10,14 @@ export default function Home() {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
 
+
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" );
+      const res = await axios.get("/posts" + search);
       setPosts(res.data);
     };
     fetchPosts();
-  }, []);
+  }, [search]);
   return (
     <>
       <Header />
