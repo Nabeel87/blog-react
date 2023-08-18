f31d4d6f0189c6dfd34b6b929323d5b2fd6b5cfb
Nabell87 <nabilsabir786@gmail.com>
2023-08-18 17:33:26 +0500

    Update Login.jsx
    login using context

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,52 @@
 import { Link } from "react-router-dom";
 import "./login.css";
+import { useContext, useRef } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Login() {
+
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" })
+    }
+    
+  };
+  console.log(isFetching);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="Enter your Username..."
+          ref={userRef}
+        />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+        />
         <button className="loginButton">Login</button>
       </form>
       <Link className="link" to="/register">
-        <button className="loginRegisterButton">Register</button>
+        <button className="loginRegisterButton" type="submit">Register</button>
       </Link>
     </div>
   );
