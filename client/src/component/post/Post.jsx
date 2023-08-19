d8c7bfbd66cc89f84d92d2e1ec06b176c2f0ee71
Nabell87 <nabilsabir786@gmail.com>
2023-08-19 13:43:39 +0500

    add liabrary
    add liabray
    yarn add path
    for accessing photo from api

--- client/src/component/post/Post.jsx
+++ client/src/component/post/Post.jsx
@@ -1,10 +1,11 @@
 import "./post.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
+    const PF = "localhost:5000/images/";
     return (
         <div className="post">
-            {post.photo && <img className="postImg" src={post.photo} alt="" />}
+            {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
             <div className="postInfo">
                 <div className="postCats">
                     {post.categories.map((c) => (
@@ -14,9 +15,9 @@ export default function Post({ post }) {
                     ))}
                 </div>
                 <Link to={`/post/${post._id}`} className="link">
-                <span className="postTitle">
-                    {post.title}
-                </span>
+                    <span className="postTitle">
+                        {post.title}
+                    </span>
                 </Link>
                 <hr />
                 <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
